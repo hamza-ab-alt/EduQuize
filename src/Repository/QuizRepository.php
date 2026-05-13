4236e84f240e30e4a6113504bb8295aee3e87685
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-13 16:10:48 +0100

    add questionrepository code

--- src/Repository/QuizRepository.php
+++ src/Repository/QuizRepository.php
@@ -10,14 +10,14 @@ class QuizRepository {
     public function __construct($db) {
         $this->db = $db;
     }
+
     public function findByCode($code) {
-        $query = "SELECT * FROM quizes WHERE code_quiz = :code LIMIT 1";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':code', $code);
-        $stmt->execute();
-        $data = $stmt->fetch();
-        if ($data) {
-            return new Quiz((int)$data['id'], $data['title'], $data['code_quiz']);
+        $stmt = $this->db->prepare("SELECT * FROM quizzes WHERE code_quiz = :code LIMIT 1");
+        $stmt->execute(['code' => $code]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            return new Quiz($row['id'], $row['title'], $row['code_quiz']);
         }
         return null;
     }
