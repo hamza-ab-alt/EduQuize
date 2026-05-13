a430caed6d6167027d594fb39d44d3ffb5f7b16b
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-13 16:25:56 +0100

    add insert in database for test

--- sql/structur.sql
+++ sql/structur.sql
@@ -42,36 +42,20 @@ CREATE TABLE results (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
 );
--- 1. Insert l-Role (ila makanch 3ndek)
 INSERT INTO roles (label) VALUES ('Formateur'), ('Apprenant');
-
--- 2. Insert User (Formateur li gha ikoun moul l-quiz)
--- Password '123456' (haché b password_hash f PHP)
 INSERT INTO users (name, email, password, role_id) 
 VALUES ('Amine Coach', 'amine@codeacademy.ma', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 1);
-
--- 3. Insert Quiz (Axe 2 & 3)
--- Ghadi nkhdm b code_quiz 'PHP2026' bach t-tester bih f homepageS.php
 INSERT INTO quizzes (title, description, code_quiz, user_id) 
 VALUES ('Quiz PHP Basics', 'Évaluation sur les bases de PHP OOP et PDO', 'PHP2026', 1);
-
--- 4. Insert Questions (Axe 3)
--- Question 1
 INSERT INTO questions (quiz_id, question) 
 VALUES (1, 'Que signifie l''acronyme PDO en PHP ?');
-
--- Question 2
 INSERT INTO questions (quiz_id, question) 
 VALUES (1, 'Quelle méthode est utilisée pour exécuter une requête préparée ?');
-
--- 5. Insert Answers (Options pour Question 1)
 INSERT INTO answers (question_id, answer, is_correct) VALUES 
 (1, 'PHP Data Objects', 1),
 (1, 'Personal Data Object', 0),
 (1, 'PHP Database Orientation', 0),
 (1, 'Programmable Data Object', 0);
-
--- 6. Insert Answers (Options pour Question 2)
 INSERT INTO answers (question_id, answer, is_correct) VALUES 
 (2, 'query()', 0),
 (2, 'execute()', 1),
