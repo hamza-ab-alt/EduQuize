5543c7d13872273eed563047503f06b7b4b7a2fc
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-13 12:03:40 +0100

    add code to recuperer the code

--- pages/student/quiz_view.php
+++ pages/student/quiz_view.php
@@ -3,3 +3,10 @@ require_once '../../config/database.php';
 require_once '../../src/Entity/Quiz.php';
 require_once '../../src/Repository/QuizRepository.php';
 require_once '../../src/Repository/QuestionRepository.php';
+$database = new Database();
+$db = $database->getConnection();
+
+$quizRepo = new \App\Repository\QuizRepository($db);
+$questionRepo = new \App\Repository\QuestionRepository($db);
+$code = $_POST['quiz_code'] ?? '';
+$quiz = $quizRepo->findByCode($code);
