85cf1d30c886ad467e3966b0c374ba31cdcf9dcf
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-13 11:58:30 +0100

    require the fils in quiz_view.php

---
+++ pages/student/quiz_view.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../../config/database.php';
+require_once '../../src/Entity/Quiz.php';
+require_once '../../src/Repository/QuizRepository.php';
+require_once '../../src/Repository/QuestionRepository.php';
