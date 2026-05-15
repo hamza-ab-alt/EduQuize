1d054a74a7dce6d0697b1cf4cbeb652e6c54953b
sara charafi <elkbircharafi8@gmail.com>
2026-05-15 10:49:24 +0100

    feat(user): create User entity class with encapsulated attributes

--- src/entity/user.php
+++ src/entity/user.php
@@ -6,6 +6,7 @@ class User{
     private $role_id;
     public function __construct($name,$email,$role_id)
     {
+        
        $this->name=$name;
        $this->email=$email;
        $this->role_id=$role_id;
